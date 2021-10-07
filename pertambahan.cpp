615bfba1520d5896161e437a09947dd3ec94e3bf
Laymoth <laymoth@pm.me>
2021-10-08 04:13:20 +0700

    Create pertambahan.cpp

---
+++ pertambahan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Input 1: ";
+  cin >> x;
+  cout << "Input 2: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Hasil: " << sum;
+  return 0;
+}
